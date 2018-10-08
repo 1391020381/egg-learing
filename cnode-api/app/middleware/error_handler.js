df1262e30edb287788ff06241ef0154023a68d59
zhoujin <zhoujin@iboxpay.com>
2018-10-08 14:51:36 +0800

    eslint

---
+++ cnode-api/app/middleware/error_handler.js
@@ -0,0 +1,19 @@
+'use strict';
+module.exports = (option, app) => {
+  return async function(ctx, next) {
+    try {
+      await next();
+    } catch (err) {
+      // 所有的异常都在 app 上出发一个error事件,框架会记录一条错误日志
+      app.emit('error', err, this);
+      const status = err.status || 500;
+      const error = status === 500 && app.config.env === 'prod' ? 'Internal Server Error' : err.message;
+      ctx.body = { error };
+      if (status === 422) {
+        ctx.body.detail = err.errors;
+      }
+      ctx.status = status;
+    }
+  };
+}
+;

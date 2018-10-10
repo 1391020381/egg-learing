7b2f458da70db3f83ccc8c552b8e4e8c8acad7d6
zhoujin <zhoujin@iboxpay.com>
2018-10-10 14:19:16 +0800

    update

--- sequelize-project/app/model/index.js
+++ sequelize-project/app/model/index.js
@@ -6,6 +6,7 @@ const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/..\..\database\config.json')[env];
+// const config = require(path.resolve(__dirname, '../../database/config.json'))[env];
 const db = {};
 
 let sequelize;
@@ -21,7 +22,7 @@ fs
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
   .forEach(file => {
-    const model = sequelize['import'](path.join(__dirname, file));
+    const model = sequelize.import(path.join(__dirname, file));
     db[model.name] = model;
   });
 
--- sequelize-project/app/model/user.js
+++ sequelize-project/app/model/user.js
@@ -1,12 +1,12 @@
 'use strict';
 module.exports = app => {
-    const { STRING, INTEGER } = app.Sequelize;
-    const User = app.modle.define('user', {
-        id: { type: INTEGER, primaryKey: true, autoIncrement: true },
-        name: STRING(30),
-        age: INTEGER,
-        created_at: DATE,
-        updated_at: DATE,
-    });
-    return User;
-}
+  const { STRING, INTEGER, DATE } = app.Sequelize;
+  const User = app.modle.define('user', {
+    id: { type: INTEGER, primaryKey: true, autoIncrement: true },
+    name: STRING(30),
+    age: INTEGER,
+    created_at: DATE,
+    updated_at: DATE,
+  });
+  return User;
+};

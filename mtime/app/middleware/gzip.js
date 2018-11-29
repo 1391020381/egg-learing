77bf27168e0decb983fda938ba1589c8c113cc2c
zhoujin <zhoujin@iboxpay.com>
2018-11-29 14:50:02 +0800

    mtime

---
+++ mtime/app/middleware/gzip.js
@@ -0,0 +1,17 @@
+'use strict';
+const isJson = require('koa-is-json');
+const zlib = require('zlib');
+
+async function gzip(ctx, next) {
+  await next();
+  let body = ctx.body;
+  if (!body) return;
+  if (isJson(body)) {
+    body = JSON.stringify(body);
+  }
+  const stream = zlib.createGzip();
+  stream.end(body);
+  ctx.body = stream;
+  ctx.set('Content-Encoding', 'gzip');
+}
+module.exports = gzip;

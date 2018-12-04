7bdc6add78cd8fdf2d444db5ad170494483370ea
zhoujin <zhoujin@iboxpay.com>
2018-12-04 14:47:19 +0800

    添加 react-redux

--- instagram/client/src/App.js
+++ instagram/client/src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux'
 import logo from './logo.svg';
 import './App.css';
+
+import store from './store/index.js'
 
 import Instagram from './pages/index.js'
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <Provider store={store}>
         <Instagram />
-      </div>
+      </Provider>
     );
   }
 }

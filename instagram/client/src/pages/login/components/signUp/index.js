6e987585ced50cf675da21a6783d28a113204958
zhoujin <zhoujin@iboxpay.com>
2018-12-04 17:55:10 +0800

    antd引入失败,切换node版本node-sass报错

--- instagram/client/src/pages/login/components/signUp/index.js
+++ instagram/client/src/pages/login/components/signUp/index.js
@@ -19,7 +19,11 @@ class SignUp extends Component {
 
     }
     render() {// getFieldDecorator
-        // const { getFieldDecorator } = this.props.form
+        const {
+            getFieldDecorator, getFieldsError, getFieldError, isFieldTouched,
+        } = this.props.form;
+        const userNameError = isFieldTouched('userName') && getFieldError('userName');
+        const passwordError = isFieldTouched('password') && getFieldError('password');
         return (
             <section className={Style.signup}>
                 <h1 className="header">
@@ -27,9 +31,22 @@ class SignUp extends Component {
                 </h1>
                 <h2 className="slogan">注册instagram分享精彩视界</h2>
                 <Button type="primary" htmlType="submit" className="facebook-login"> 使用Facebook登陆</Button>
-                <div className="or-line"></div>
+                <div className="or-line">
+                    <span className="line"></span>
+                    <span className="name">或</span>
+                    <span className="line"></span>
+                </div>
                 <Form className="register-form" onSubmit={this.handleSubmit.bind(this)}>
-
+                    <FormItem
+                        validateStatus={userNameError ? 'error' : ''}
+                        help={userNameError || ''}
+                    >
+                        {getFieldDecorator('userName', {
+                            rules: [{ required: true, message: 'Please input your username!' }],
+                        })(
+                            <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+                        )}
+                    </FormItem>
                 </Form>
             </section>
         )

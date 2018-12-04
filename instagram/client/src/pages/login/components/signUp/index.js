37daf8eadb0e29adacbe538b35339f4a463276dd
zhoujin <zhoujin@iboxpay.com>
2018-12-04 17:32:31 +0800

    登陆注册切换

--- instagram/client/src/pages/login/components/signUp/index.js
+++ instagram/client/src/pages/login/components/signUp/index.js
@@ -2,11 +2,36 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 
-import './index.scss'
+import { Form, Icon, Input, Button, notification } from 'antd'
+import Style from './index.scss'
+
+const FormItem = Form.Item
 class SignUp extends Component {
-    render() {
+    constructor(props) {
+        super(props)
+        this.state = {}
+        this.handleSubmit.bind(this)
+    }
+    handleSubmit(e) {
+
+    }
+    onChangeHandler(type, event) {
+
+    }
+    render() {// getFieldDecorator
+        // const { getFieldDecorator } = this.props.form
         return (
-            <div>signup</div>
+            <section className={Style.signup}>
+                <h1 className="header">
+                    <span className="instagram"></span>
+                </h1>
+                <h2 className="slogan">注册instagram分享精彩视界</h2>
+                <Button type="primary" htmlType="submit" className="facebook-login"> 使用Facebook登陆</Button>
+                <div className="or-line"></div>
+                <Form className="register-form" onSubmit={this.handleSubmit.bind(this)}>
+
+                </Form>
+            </section>
         )
     }
 }
--- instagram/client/src/pages/login/index.js
+++ instagram/client/src/pages/login/index.js
@@ -10,17 +10,39 @@ import SingnUp from './components/signUp'
 import Footer from '../../components/footer/index.js'
 
 class Login extends Component {
-
+    constructor(props) {
+        super(props)
+        this.state = {
+            isSignUp: true
+        }
+    }
+    toggleSign() {
+        this.setState({
+            isSignUp: !this.state.isSignUp
+        })
+    }
     render() {
         return (
-            <main class={Style.login}>
+            <main className={Style.login}>
                 <article className="login_info">
                     <section className="descript">
                         <div className="photo"></div>
                     </section>
                     <section className="login_dialog">
-                        <SingnIn />
-                        <SingnUp />
+                        {
+                            this.state.isSignUp
+                                ? <SingnIn />
+                                : <SingnUp toggleSign={this.toggleSign.bind(this)} />
+
+                        }
+                        <div className="toggle_ways">
+                            {
+                                this.state.isSignUp
+                                    ? <span>没有账号? <a className="notice" onClick={this.toggleSign.bind(this)}>注册</a></span>
+                                    : <span>有账号了?<a className="notice" onClick={this.toggleSign.bind(this)}>请登录</a></span>
+                            }
+
+                        </div>
                     </section>
                 </article>
                 <Footer />

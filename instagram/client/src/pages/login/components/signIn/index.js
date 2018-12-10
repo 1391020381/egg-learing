49c0be3ee73b9c58f3afd8214b88ec71a5f71e41
zhoujin <zhoujin@iboxpay.com>
2018-12-10 10:51:28 +0800

    update-login

--- instagram/client/src/pages/login/components/signIn/index.js
+++ instagram/client/src/pages/login/components/signIn/index.js
@@ -2,13 +2,23 @@ import React, { Component } from 'react'
 import Proptypes from 'prop-types'
 import { connect } from 'react-redux'
 
-import './index.scss'
+
+import { Form, Icon, Input, Button, notification } from 'antd'
+import Style from './index.scss'
+
+const FormItem = Form.Item;
 class SignIn extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
     render() {
         return (
-            <div>
-                SignIn
-            </div>
+            <section className={Style.signup}>
+                <h1 className="header">
+                    <span className="instagram"></span>
+                </h1>
+            </section>
         )
     }
 }
